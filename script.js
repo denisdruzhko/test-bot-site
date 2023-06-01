26433542f031732db4a5515321c6e9663f0bfb68
mvper7 <71771601+denisdruzhko@users.noreply.github.com>
2023-06-01 21:40:44 +0300

    Update script.js

--- script.js
+++ script.js
@@ -4,6 +4,9 @@ tg.expand();
 
 let btn = document.querySelector('#btn');
 
+tg.MainButton.textColor = "#FFFFFF";
+tg.MainButton.color = "#FF00FF";
+
 btn.addEventListener("click", function(){
         tg.MainButton.setText("Message was sent");
         tg.MainButton.show();
